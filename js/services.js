7fe566fd5f2e2c920fde1639fd04e3cbfd41541d
shevchenkoak <demorest49de@gmail.com>
2022-05-01 17:05:26 +0300

    c

--- js/services.js
+++ js/services.js
@@ -1,5 +1,6 @@
 const _API_KEY = 'b4326da2f71741f2fd37fd13cda904d9'
 const _BASE_URL = 'https://api.themoviedb.org/3'
+const _LANGUAGE = '&language=ru-RU'
 
 /*trending/all/day?api_key=<<api_key>>*/
 
@@ -13,8 +14,7 @@ const getData = url => fetch(url)
     .catch(err => console.error(err))
 
 
-export const getTriends = async () => {
-    const url = `${_BASE_URL}/trending/all/day?api_key={b4326da2f71741f2fd37fd13cda904d9}`
-    const data = await getData('')
-    console.log('data: ', data)
+export const getTriends = async (type = 'all', period = 'week', page = 1) => {
+    const url = `${_BASE_URL}/trending/${type}/${period}?api_key=${_API_KEY}${_LANGUAGE}&page=${page}`
+    return await getData(url)
 }
