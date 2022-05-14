bc42ce77ae2d4d326de0c5fbdce78a6c638a09eb
shevchenkoak <demorest49de@gmail.com>
2022-05-14 13:59:47 +0300

    c

--- js/menuLink.js
+++ js/menuLink.js
@@ -8,9 +8,20 @@ import renderCard from "./renderCard.js"
 const otherFilmsTitle = document.querySelector('.other-films__title')
 const filmWeek = document.querySelector('.film-week')
 const getNav = document.querySelectorAll('.get-nav')
-console.log('getNav: ',getNav)
-const menuLink = ()=>{
+console.log('getNav: ', getNav)
 
+const menuLink = () => {// отличие форич от мап. форич ничего не возвращает, мап - возвращает измененный элемент
+    getNav.forEach((nav) => {
+        nav.addEventListener('click', event => {
+
+            const target = event.target.closest('.get-nav__link') // отбор куда кликать и что показывать через closest()
+
+            if (target) {
+                event.preventDefault()
+                filmWeek.style.display = 'none'
+            }
+        })
+    })
 }
 
-export  default menuLink
+export default menuLink
